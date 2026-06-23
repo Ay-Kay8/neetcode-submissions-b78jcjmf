78b0e36a1e1346d117fb3deedc25aba50caf7d15
Thanh <134972308+Ay-Kay8@users.noreply.github.com>
2026-06-23 09:57:22 -0400

    Add: course-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-0.py
@@ -0,0 +1,42 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+
+        # 1. create graph
+        graph = dict([(i, []) for i in range(numCourses)])
+        for destination, source in prerequisites:
+            graph[source].append(destination)
+
+        count = 0
+        for val in graph.values():
+            if len(val) == 0:
+                count += 1
+
+        # 2. DFS algorithmn
+        visited = set() # nodes with no cycles
+        visiting = set()
+        valid_ordering = []
+
+        def has_cycles(node):
+
+            if node in visited:
+                return False
+            if node in visiting:
+                return True
+            
+            visiting.add(node)
+
+            for neighbor in graph[node]:
+                if has_cycles(neighbor):
+                    return True
+
+            visiting.remove(node)
+            visited.add(node)
+            # Topological sort
+            valid_ordering.insert(0, node) # append at the start 
+
+        # 3. Apply it to all nodes (in case of disconnected graphs)
+        for node in graph:
+            # if node is already in visited, it'll return False right away
+            if has_cycles(node):
+                return []
+        return valid_ordering
